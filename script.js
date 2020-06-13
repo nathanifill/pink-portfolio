67b1af62c09afe236a9cac977c8b61f39270971c
Nathan Ifill <nathan.ifill@live.co.uk>
2020-06-13 14:45:21 +0100

    Initialise repo

---
+++ script.js
@@ -0,0 +1,36 @@
+// Change year in footer to current year
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+// Direct clicks on buttons to correct pages
+
+document.getElementById('live-demo-btn-1').onclick = function () {
+  location.href = 'https://nathanifill.github.io/exchange-rate-converter/';
+};
+
+document.getElementById('github-repo-btn-1').onclick = function () {
+  location.href = 'https://github.com/nathanifill/exchange-rate-converter';
+};
+
+document.getElementById('live-demo-btn-2').onclick = function () {
+  location.href = 'https://nathanifill.github.io/movie-seat-booking/';
+};
+
+document.getElementById('github-repo-btn-2').onclick = function () {
+  location.href = 'https://github.com/nathanifill/movie-seat-booking';
+};
+
+document.getElementById('live-demo-btn-3').onclick = function () {
+  location.href = 'https://nathanifill.github.io/space-camp-form-validation/';
+};
+
+document.getElementById('github-repo-btn-3').onclick = function () {
+  location.href = 'https://github.com/nathanifill/space-camp-form-validation';
+};
+
+document.getElementById('live-demo-btn-4').onclick = function () {
+  location.href = 'https://nathanifill.github.io/custom-video-player/';
+};
+
+document.getElementById('github-repo-btn-4').onclick = function () {
+  location.href = 'https://github.com/nathanifill/custom-video-player';
+};
